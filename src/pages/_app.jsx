7fd066c3366365afd85963e444341d59b091391f
Sam Rapaport <me@samrapaport.com>
2022-10-30 20:30:31 -0700

    Add gtag.

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,27 @@
 import 'focus-visible'
 import '@/styles/tailwind.css'
+import Script from 'next/script'
 import { Header } from '@/components/Header'
 import { Footer } from '@/components/Footer'
 
 export default function App({ Component, pageProps }) {
   return (<>
+    {/* Global Site Tag (gtag.js) - Google Analytics */}
+    <Script
+      strategy="lazyOnload"
+      src={`https://www.googletagmanager.com/gtag/js?id=G-HPKCKKH20R`}
+    />
+
+    <Script strategy="lazyOnload">
+      {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-HPKCKKH20R');
+      `}
+    </Script>
+
     <Header />
     <Component {...pageProps} />
     <Footer />
