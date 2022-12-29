d17cf8c530521176a1ff163aa53b79234c5a45b3
Sam Rapaport <me@samrapaport.com>
2022-12-28 21:20:57 -0800

    Add contact page.

---
+++ pages/contact.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Head>
+      <Title>Contact</Title>
+    </Head>
+
+    <DopeHero title="Create with us &#10036;&#xFE0E;&nbsp;"> Are you ready to build something Dope? </DopeHero>
+
+    <DopeSection>
+      <DopeContainer>
+        <p class="mb-8 max-w-5xl text-lg font-bold">
+          We're here for it. Schedule a call with our team or email us at
+          <span class="underline">hey@thatsrdd.com</span>.
+        </p>
+        <DopeButton to="https://calendly.com/samrapaport" target="_blank">Schedule a Call</DopeButton>
+      </DopeContainer>
+    </DopeSection>
+  </div>
+</template>
