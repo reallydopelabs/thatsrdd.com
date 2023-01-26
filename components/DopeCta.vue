2ce0e2b1e07198ad51d6a5f1c840689f2fc0eec5
Sam Rapaport <me@samrapaport.com>
2023-01-25 16:07:15 -0800

    Clean up CTA layout.

--- components/DopeCta.vue
+++ components/DopeCta.vue
@@ -1,11 +1,11 @@
 <template>
   <DopeSection class="border-t border-neutral-900">
     <DopeContainer>
-      <div class="text-center md:flex md:items-center md:justify-between">
+      <div class="text-center md:flex md:items-center md:justify-between md:text-left">
         <div class="max-w-2xl flex-grow">
-          <DopeTagline class="mb-8">Are you ready to build <span data-chaffle>something dope</span>?</DopeTagline>
+          <DopeTagline class="mb-8 md:mb-0">Are you ready to build <span data-chaffle>something dope</span>?</DopeTagline>
         </div>
-        <DopeButton to="/contact">Get in touch</DopeButton>
+        <DopeButton to="/contact" class="shrink-0">Get in touch</DopeButton>
       </div>
     </DopeContainer>
   </DopeSection>
