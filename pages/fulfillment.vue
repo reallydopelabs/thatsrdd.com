dbea34304c4b8212df4c640a7bf9a58097c92cff
Sam Rapaport <me@samrapaport.com>
2025-03-13 12:37:58 -0700

    Add fulfillment page.

---
+++ pages/fulfillment.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Head>
+      <Title>Fulfillment Policy</Title>
+    </Head>
+
+    <section>
+      <h1>Fulfillment Policy</h1>
+
+      Last Updated: 2025-03-10 Order Processing & Shipping • We do not sell any physical products • Digital services
+      will be delivered as agreed upon based on the scope of work • For service subscriptions, services will be
+      delivered as requested throughout the duration of the subcription Refund Policy • Digital products/services:
+      Refunds are not offered. Cancellation Policy • Subscription services can be canceled at any time before the next
+      billing cycle by contacting hey@thatsrdd.com or your RDD representative directly. • Subscription services may also
+      be canceled via the direct billing portal link sent from our team when your subscription began Contact Us If you
+      have any questions, reach out to hey@thatsrdd.com.
+    </section>
+  </div>
+</template>
